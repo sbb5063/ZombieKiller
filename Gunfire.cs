e446aac1964e99a45b0cc1cc72e1ed061bb0e3a7
sbb5063 <74914158+sbb5063@users.noreply.github.com>
2020-11-26 14:13:34 +0530

    Add files via upload

---
+++ Gunfire.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gunfire : MonoBehaviour
+{
+    Animation GunshootAnim;
+    AudioSource GunShoot;
+    public GameObject Flash;
+    public GameObject realSMG;
+    public GameObject gunlabel;
+    void Start()
+    {
+        GunshootAnim = GetComponent<Animation>();
+        GunShoot = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if(GlobalAmmo.LoadedAmmo >= 1)
+       {
+            if(Input.GetMouseButtonDown(0))
+            {
+               GunShoot.Play();
+               Flash.SetActive(true);
+               Invoke("MuzzleOff", 0.1f);
+               GunshootAnim.Play("Gunshoot");
+               GlobalAmmo.LoadedAmmo --;
+            }
+       }
+
+       if(realSMG.activeInHierarchy)
+       {
+           gunlabel.SetActive(false);
+       }
+
+    }
+
+    void MuzzleOff()
+    {
+        Flash.SetActive(false);
+    }   
+}
+ 
---
+++ HandGunRelaod.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandGunRelaod : MonoBehaviour
+{
+    public AudioSource RelaodSound;
+    public GameObject CrossObject;
+    public GameObject MechanicsObject;
+    int ClipCount;
+    int ReserveCount;
+    int ReloadAvailable;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ClipCount = GlobalAmmo.LoadedAmmo;
+        ReserveCount = GlobalAmmo.CurrentAmmo;
+        if(ReserveCount == 0)
+        {
+           ReloadAvailable = 0;
+        }
+        else
+        {
+            ReloadAvailable = 10 - ClipCount;
+        }
+
+        if(Input.GetButtonDown("Relaod"))
+        {
+            if(ReloadAvailable >= 1)
+            {
+                if(ReserveCount <= ReloadAvailable)
+                {
+                    GlobalAmmo.LoadedAmmo += ReserveCount; 
+                    GlobalAmmo.CurrentAmmo -= ReserveCount;
+                    ActionReload();
+                }
+                else
+                {
+                    GlobalAmmo.LoadedAmmo += ReloadAvailable; 
+                    GlobalAmmo.CurrentAmmo -= ReloadAvailable;
+                    ActionReload();
+                }
+
+            }    
+        }
+
+        Invoke("EnableScripts", 1.1f);
+    }
+
+    void ActionReload()
+    {
+        this.GetComponent<Gunfire>().enabled = false;
+        CrossObject.SetActive (false);
+        MechanicsObject.SetActive (false);
+        RelaodSound.Play();
+        GetComponent<Animation>().Play("HandGunRelaod");
+    }
+
+    void EnableScripts()
+    {
+        this.GetComponent<Gunfire>().enabled = true;
+        CrossObject.SetActive (true);
+        MechanicsObject.SetActive (true);
+    }
+}
