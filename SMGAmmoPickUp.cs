47e81e9616004add0a0e69b8a47cfa54bc4b8c6e
sbb5063 <74914158+sbb5063@users.noreply.github.com>
2020-11-26 14:15:14 +0530

    Add files via upload

---
+++ SMGAmmoPickUp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMGAmmoPickUp : MonoBehaviour
+{
+    public AudioSource Ammopick;
+    
+    
+    void Start()
+    {
+        
+    }
+       
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Ammopick.Play();
+        if(GlobalAmmo.LoadedAmmo == 0)
+        {
+            GlobalAmmo.LoadedAmmo += 30;
+            this.gameObject.SetActive(false);
+        }
+        else
+        {
+            GlobalAmmo.CurrentAmmo += 30;
+            this.gameObject.SetActive(false);
+        }    
+    }   
+}
---
+++ SMGFire.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMGFire : MonoBehaviour
+{
+    public GameObject TheSMG;
+    public GameObject MuzzleFlash;
+    public AudioSource SMGSound;
+    //private int AmmoCount;
+    private int firing;
+
+    public GameObject CH;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //AmmoCount = GlobalAmmo.LoadedAmmo;
+       
+       if(GlobalAmmo.LoadedAmmo >= 1)
+       {
+          if(Input.GetMouseButtonDown(0))
+          {
+            if(firing == 0)
+            {
+              StartCoroutine("SMGFirre");
+            }
+          }
+       }
+    }
+
+    IEnumerator SMGFirre()
+    {
+        firing = 1;
+        CH.GetComponent<Animator>().enabled = true;
+        GlobalAmmo.LoadedAmmo -= 1;
+        SMGSound.Play();
+        MuzzleFlash.SetActive(true);
+        TheSMG.GetComponent<Animator>().enabled = true;
+        
+        yield return new WaitForSeconds(0.1f);
+        
+        MuzzleFlash.SetActive(false);
+        TheSMG.GetComponent<Animator>().enabled = false;
+        CH.GetComponent<Animator>().enabled = false;
+        firing = 0;
+    }
+}
