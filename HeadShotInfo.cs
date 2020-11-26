c51dcda132c315b48eccfc4aee6d584e9ada4b11
sbb5063 <74914158+sbb5063@users.noreply.github.com>
2020-11-26 14:09:12 +0530

    Add files via upload

---
+++ HeadShotInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeadShotInfo : MonoBehaviour
+{
+    float TheDistance = PlayerCast.DistanceFromTarget;
+    public Text InfoText;
+
+    void Update()
+    {
+        TheDistance = PlayerCast.DistanceFromTarget;
+    }
+  
+  
+  void OnMouseOver()
+   {
+       if(TheDistance <= 2)
+       {
+          StartCoroutine(Headshotinfo());
+       }
+   }
+
+   IEnumerator Headshotinfo()
+   {
+       InfoText.text = "Shooting The Enemies In Head \n Kills Them Imediately";
+       yield return new WaitForSeconds(5f);
+       InfoText.text = "";
+       Destroy(gameObject);
+   }
+}
